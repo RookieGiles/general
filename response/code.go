1e0c5d1c05718aafec23a0da704f00d421b8faf5
WangGeng <giles.wang@qq.com>
2023-11-15 19:52:45 +0800

    init

--- response/code.go
+++ response/code.go
@@ -1,6 +1,6 @@
 package response
 
 var (
-	SuccessCode int    = 0         //请求成功返回code 码
-	SuccessMsg  string = "Success" // 请求成功返回描述信息
+	successCode int    = 0         //请求成功返回code 码
+	successMsg  string = "Success" // 请求成功返回描述信息
 )
--- response/response.go
+++ response/response.go
@@ -1,26 +1,26 @@
 package response
 
-type SuccessStruct struct {
+type successStruct struct {
 	Code int         `json:"code"`
 	Msg  interface{} `json:"msg"`
 	Data interface{} `json:"data"`
 }
 
-type FailureStruct struct {
+type failureStruct struct {
 	Code int         `json:"code"`
 	Msg  interface{} `json:"msg"`
 }
 
 // Success 请求成功返回
-func Success(data interface{}) *SuccessStruct {
-	json := &SuccessStruct{Code: SuccessCode, Msg: SuccessMsg, Data: data}
+func Success(data interface{}) *successStruct {
+	json := &successStruct{Code: successCode, Msg: successMsg, Data: data}
 
 	return json
 }
 
 // Failure 请求失败返回
-func Failure(code int, data interface{}) *FailureStruct {
-	json := &FailureStruct{Code: SuccessCode, Msg: SuccessMsg}
+func Failure(code int, data interface{}) *failureStruct {
+	json := &failureStruct{Code: successCode, Msg: successMsg}
 
 	return json
 }
